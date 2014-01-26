719ccd3eddca6294e8ca702eb4f194a470112345
Sergio Agosti <sagosti@theframeworks.com>
2014-01-26 22:52:14 +0000

    a big refactor

--- config.rb
+++ config.rb
@@ -40,6 +40,9 @@ activate :automatic_image_sizes
 # Reload the browser automatically whenever files change
 activate :livereload
 
+# http://bourbon.io
+activate :bourbon
+
 # Methods defined in the helpers block are available in templates
 # helpers do
 #   def some_helper
